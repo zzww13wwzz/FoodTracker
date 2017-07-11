587ff27e46a955a9f7543ce39400b8f4d712088a
Viktoriia Vovk <zzww13wwzz@gmail.com>
2017-07-11 22:21:37 +0300

    Create project + add storyboard

---
+++ FoodTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FoodTracker
+//
+//  Created by Viktoriia Vovk on 7/11/17.
+//  Copyright © 2017 Viktoriia Vovk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
